78a0618c6402dcf08618f82c0f7c8a54147c6e19
Mashoto Makobe <mashoto.makobe99@gmail.com>
2025-07-19 13:31:32 +0200

    Update quality_checks_gold.sql

--- tests/quality_checks_gold.sql
+++ tests/quality_checks_gold.sql
@@ -13,7 +13,6 @@ Usage Notes:
     - Investigate and resolve any discrepancies found during the checks.
 ===============================================================================
 */
-
 -- ====================================================================
 -- Checking 'gold.dim_customers'
 -- ====================================================================
@@ -44,8 +43,8 @@ HAVING COUNT(*) > 1;
 -- Check the data model connectivity between fact and dimensions
 SELECT * 
 FROM gold.fact_sales f
-LEFT JOIN gold.dim_customers c
-ON c.customer_key = f.customer_key
-LEFT JOIN gold.dim_products p
-ON p.product_key = f.product_key
-WHERE p.product_key IS NULL OR c.customer_key IS NULL  
+LEFT JOIN    gold.dim_customers c
+ON           c.customer_key = f.customer_key
+LEFT JOIN    gold.dim_products p
+ON           p.product_key = f.product_key
+WHERE p.product_key IS NULL OR c.customer_key IS NULL;
