be216e6eecf40f031ee186bffab11e177d7de773
PreciousByte <precious.mp43@gmail.com>
2025-07-19 12:45:13 +0200

    Update ddl_silver.sql

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -84,7 +84,7 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
     cat                 NVARCHAR(50),
     subcat              NVARCHAR(50),
     maintenance         NVARCHAR(50),
-	dwh_create_date     DATETIME2 DEFAULT GETDATE()
+    dwh_create_date     DATETIME2 DEFAULT GETDATE()
 );
 GO
 
